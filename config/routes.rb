51c94db17a96d4c552ff039b9a4d6be0b59bd212
root <root@DESKTOP-EQ2OFGT.localdomain>
2018-02-11 22:04:01 -0600

    patch de links

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
  
 
-  get 'rooms/index'
+ # get 'contactus/index'
+
+ # get 'rooms/index'
 
   resources :users
   resources :sessions
   resources :rooms
-
+  resources :contactus
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
